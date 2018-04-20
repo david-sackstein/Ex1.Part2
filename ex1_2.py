1b35b34b31f59e1efa4d91c3ba84e3b75eb6a5a9
david-sackstein <davids@codeprecise.com>
2018-04-20 19:16:12 +0300

    Reading of data file

--- ex1_2.py
+++ ex1_2.py
@@ -1,15 +1,30 @@
-def read_rows():
+import os
+
+import numpy
+from numpy.core.multiarray import ndarray
+from numpy.linalg import svd, norm
+from matplotlib.pyplot import *
+import scipy.misc as misc
+
+
+def read_rows(row_count):
     headers = {}
-    rows = []
+    matrix = None
+    row_index = 0
     for line_index, line in enumerate(lines):
         values = line.split(',')
         if not headers:
             for i, value in enumerate(values):
                 headers[value] = i
+            matrix = ndarray((row_count, len(headers)))
         else:
             if len(values) == len(headers):
-                rows.append(values)
-    return headers, rows
+                for i in range(0, len(values)):
+                    matrix[row_index, i] = float(values[i].strip('\"')[:4])
+                row_index += 1
+                if row_index == row_count :
+                    break
+    return headers, matrix
 
 
 if __name__=='__main__':
@@ -21,7 +36,13 @@ if __name__=='__main__':
     except:
         print('Failed to open ' + file_name)
 
-    headers, rows = read_rows()
+    headers, matrix = read_rows(100)
 
-    for row in rows:
-        print(row[headers['lat']])
+    for row in range(len(matrix)):
+        print(matrix[row, headers['lat']])
+
+    decomposed = svd(matrix)
+    u, s, vh = decomposed
+    t = u[:, :matrix.shape[1]]
+    ok = np.allclose(matrix, np.dot(t * s, vh))
+    print(ok)
